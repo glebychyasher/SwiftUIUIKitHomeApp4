01d7796e76e170fb891e088b85f7fcbb8004b2ad
glebychyasher <zobningleb01@icloud.com>
2023-12-24 23:48:10 +0300

    Initial Commit

---
+++ SwiftUIUIKitHomeApp4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIUIKitHomeApp4
+//
+//  Created by Глеб Зобнин on 24.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIUIKitHomeApp4/SwiftUIUIKitHomeApp4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUIKitHomeApp4App.swift
+//  SwiftUIUIKitHomeApp4
+//
+//  Created by Глеб Зобнин on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUIKitHomeApp4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
